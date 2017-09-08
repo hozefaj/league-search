24db5dee0541a564aca377ce0f4ee9ca972ea9da
leonardthang <leonard.thang@sjsu.edu>
2017-09-08 12:54:54 -0700

    Changed file structure

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import '../css/App.css';
+
+class App extends Component {
+
+    state = {
+        summoner: 'lol',
+        searchTerm: ''
+    }
+/*
+    searchThang = () => {
+        const url = 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/Thang?api_key=RGAPI-a0498d0a-fc2b-4af0-a032-3b9b14d0f2ad';
+        axios.get(url).then(res => {
+           this.setState({
+                summoner: res.data.name
+           })
+            console.log(res);
+        });
+    }
+
+*/
+    render() {
+    return (
+      <div className="App">
+
+          {this.state.summoner}
+      </div>
+    );
+    }
+}
+
+export default App;
