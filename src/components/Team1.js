0cbdaaba45b8bb1582c510eebaf1c8dc456ac402
leonardthang <leonard.thang@sjsu.edu>
2017-09-09 01:03:41 -0700

    Team1 and Team2 pass a prop to TeamTable

--- src/components/Team1.js
+++ src/components/Team1.js
@@ -2,7 +2,10 @@ import React from 'react';
 import TeamTable from './TeamTable';
 
 const Team1 = ({data}) => (
-    <TeamTable data={data}/>
+    <TeamTable
+        data={data}
+        team={1}
+    />
 );
 
 export default Team1;
--- src/components/Team2.js
+++ src/components/Team2.js
@@ -2,7 +2,10 @@ import React from 'react';
 import TeamTable from './TeamTable';
 
 const Team2 = ({data}) => (
-    <TeamTable data={data}/>
+    <TeamTable
+        data={data}
+        team={2}
+    />
 );
 
 export default Team2;
