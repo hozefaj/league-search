14d72fea4c6308335bb1f3fbc01e245b632de80c
leonardthang <leonard.thang@sjsu.edu>
2017-09-09 00:10:26 -0700

    Added team1 & team2 components

---
+++ src/components/Team1.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import TeamTable from './TeamTable';
+
+const Team1 = ({data}) => (
+    <TeamTable data={data}/>
+);
+
+export default Team1;
---
+++ src/components/Team2.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import TeamTable from './TeamTable';
+
+const Team2 = ({data}) => (
+    <TeamTable data={data}/>
+);
+
+export default Team2;
