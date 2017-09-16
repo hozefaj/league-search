ec7a65ff34415b1b4ce0ea65b1e71e4a27dacc51
leonardthang <leonard.thang@sjsu.edu>
2017-09-15 17:16:04 -0700

    Seperated the two API calls

--- server/server.js
+++ server/server.js
@@ -7,16 +7,22 @@ const port = process.env.PORT || 3001; // Set the port to 3001
 
 app.listen(port);
 
-app.get('/api/:summoner', (req, res) => {
-    const { summoner } = req.params;
+app.get('/api/:summoner/', (req, res) => {
+    const {summoner} = req.params;
     const url = `https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/${summoner}?api_key=${RIOT_API_KEY}`;
 
     request(url, (error, response, body) => {
-        var obj = JSON.parse(body);
-        const url2 = `https://na1.api.riotgames.com/lol/spectator/v3/active-games/by-summoner/${obj.id}?api_key=${RIOT_API_KEY}`;
-        request(url2, (error, response, body) => {
-            res.send(body);
-        })
+        res.send(body);
     });
 });
+
+app.get('/api/active-game/:id', (req, res) => {
+   const { id } = req.params;
+   const url = `https://na1.api.riotgames.com/lol/spectator/v3/active-games/by-summoner/${id}?api_key=${RIOT_API_KEY}`;
+
+   request(url, (error, response, body) => {
+      res.send(body);
+   });
+});
+
 console.log(`express app listening on port ${port}`);
