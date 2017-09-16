6b4d0d55f718549c32780d862766d4e00b0315ed
leonardthang <leonard.thang@sjsu.edu>
2017-09-16 11:42:49 -0700

    A table will only render if the data is valid

--- src/components/TeamTable.js
+++ src/components/TeamTable.js
@@ -1,35 +1,35 @@
 import React from 'react';
 import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
 import Paper from 'material-ui/Paper';
-
 import '../css/TeamTable.css'
 
 let id = 0;
 
 const TeamTable = ({data, team}) => {
-    let arr = [];
-    const participants = data.participants;
-    const teamLength = data.participants.length;
-    if (team === 1) {
-        arr = participants.slice(0, teamLength / 2);
-    }
-    if (team === 2) {
-        arr = participants.slice(teamLength / 2, teamLength);
-    }
-    return(
-        <Paper className="paper">
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell> Name </TableCell>
-                        <TableCell numeric> Champion </TableCell>
-                        <TableCell numeric> Rank </TableCell>
-                    </TableRow>
+    if (data.participants) {
+        let arr = [];
+        const participants = data.participants;
+        const teamLength = data.participants.length;
+        if (team === 1) {
+            arr = participants.slice(0, teamLength / 2);
+        }
+        if (team === 2) {
+            arr = participants.slice(teamLength / 2, teamLength);
+        }
+        return (
+            <Paper className="paper">
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell> Name </TableCell>
+                            <TableCell numeric> Champion </TableCell>
+                            <TableCell numeric> Rank </TableCell>
+                        </TableRow>
 
-                </TableHead>
+                    </TableHead>
                     <TableBody>
                         {arr.map(player => {
-                            return(
+                            return (
                                 <TableRow key={id++}>
                                     <TableCell>{player.summonerName}</TableCell>
                                     <TableCell numeric>{player.championId}</TableCell>
@@ -38,9 +38,10 @@ const TeamTable = ({data, team}) => {
                             );
                         })}
                     </TableBody>
-            </Table>
-        </Paper>
-    );
+                </Table>
+            </Paper>
+        );
+    }
 }
 
 export default TeamTable;
