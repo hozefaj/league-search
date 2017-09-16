a43400064b9deaa01c33ca75673b382d94ca00fd
leonardthang <leonard.thang@sjsu.edu>
2017-09-15 23:48:40 -0700

    Added container and material ui button

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import '../css/SearchBar.css';
+import Button from 'material-ui/Button';
 
 const SearchBar = ({onSearchChange, onSubmit}) => (
-    <form
-        className="SearchBar"
-        onSubmit={(e) => {
-            e.preventDefault();
-            onSubmit();
-        }}
-    >
-        <input
-            className="input"
-            onChange={onSearchChange}
-        >
-        </input>
-    </form>
+    <div className="container">
+        <div>
+            <form
+                className="SearchBar"
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    onSubmit();
+                }}
+            >
+                <input
+                    className="input"
+                    onChange={onSearchChange}
+                >
+                </input>
+            </form>
+        </div>
+        <div>
+            <Button className="button">
+                PRESS ME
+            </Button>
+        </div>
+    </div>
 );
 
 export default SearchBar;
