b7a806679e614f91ffa427c7825fb653215e91e7
leonardthang <leonard.thang@sjsu.edu>
2017-09-16 19:34:20 -0700

    Added alt text to img

--- src/components/TeamTable.js
+++ src/components/TeamTable.js
@@ -31,7 +31,7 @@ const TeamTable = ({data, team}) => {
                             return (
                                 <TableRow key={id++}>
                                     <TableCell>{player.name}</TableCell>
-                                    <TableCell> <img src={url} height="35" width="35"/> {player.champName} </TableCell>
+                                    <TableCell> <img src={url} height="35" width="35" alt={player.champName}/> {player.champName} </TableCell>
                                     <TableCell numeric>{1}</TableCell>
                                 </TableRow>
                             );
